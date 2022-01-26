91cd12b8efcc0b3712c81508438049034bf53814
ermaozi <admin@ermao.net>
2022-01-26 11:35:41 +0800

    新增自动命名脚本

--- cat/_rename.bat
+++ cat/_rename.bat
@@ -1,10 +1,17 @@
-@ECHO OFF
+@echo off 
+call :get "%cd%"
+pause>nul
+exit
+:get
+echo''>%~n1.list
 SETLOCAL ENABLEDELAYEDEXPANSION
 SET /A num=0
 FOR /F "tokens=*" %%i in ('dir /A-D /B /OD /TC') DO (
-IF NOT "%%i"=="%~n0%~x0" (
+IF NOT "%%i"=="%~n0%~x0" (IF NOT "%%i"=="%~n1.list" (
 SET /A num+=1
 REN "%%i" "!num!%%~xi"
+echo https://cdn.jsdelivr.net/gh/ermaozi/meme/cat/!num!%%~xi>>%~n1.list
+)
 )
 )
 ENDLOCAL
